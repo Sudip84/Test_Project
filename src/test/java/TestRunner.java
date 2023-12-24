9fbfb22145835a13f21f6fb02eca1c1eeeba2152
Sudip Patel <sudiplondon12@gmail.com>
2023-12-24 14:45:27 +0000

    modified runner class

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/resources/homePage.feature",
-        plugin = { "pretty", "html:target/cucumber-reports" },
+        plugin = { "pretty", "html:target/cucumber-reports/cucumber_report.html" },
         monochrome = true
 )
 
